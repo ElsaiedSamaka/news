c470636b1dd64b1a0daa205fc8ae8056da327481
MrAnsheff <mransheff86@gmail.com>
2018-08-30 14:01:29 +0400

    Item Model + Yaml Packages

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,19 @@
 import "package:flutter/material.dart";
 
-class App extends StatelessWidget{
-Widget build(context){
-  return MaterialApp(
-    title: 'News',
-    home: Scaffold(
-      body: Text('Just text'),
-    ),
-  );
-}
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'News',
+      home: Scaffold(
+        body: Center(
+          child: Stack(
+            children: <Widget>[
+              Text('sdfsdfsdf'),
+              RaisedButton(child: Text('sdfsdf'), onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
