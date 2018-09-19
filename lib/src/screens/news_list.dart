9f91723e2312cc1c93614c1d9063372c5ee3bed3
MrAnsheff <mransheff86@gmail.com>
2018-09-19 14:41:23 +0400

    With ListBuilder Example

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,13 +1,38 @@
 import "package:flutter/material.dart";
+import "dart:async";
 
-class NewsList extends StatelessWidget{
-  Widget build(context){
+class NewsList extends StatelessWidget {
+  Widget build(context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('News List'),
-        ),
-        body: Text('Show some news here!'),
+      ),
+      body: listBuilder(),
+    );
+  }
 
+  Widget listBuilder(){
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int indx){
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot){
+            return Container(
+              height: 80.0,
+              child: snapshot.hasData
+            ? Text('I am HERE $indx!')
+            : Text('I am not here $indx!'),);
+          },
+        );
+      },
+    );
+  }
+
+  getFuture(){
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'hi!',
     );
   }
 }
